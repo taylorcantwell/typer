c06df7b00d1eb1da32c809a99c4cd96cb333f681
Taylor <taylor@birchal.com>
2022-08-24 19:40:55 +1000

    nextjs dynamic import type fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 
-const Home = dynamic(
+const Home = dynamic<{}>(
   () => import('../components/Home/Home').then((module) => module.Home),
   { ssr: false }
 );
